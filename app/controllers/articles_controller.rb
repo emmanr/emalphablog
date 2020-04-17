9bc2f7271d00002801228b10e4a1c305dc6d859c
Emman Ruaza <emmanuel.ruaza@bibo.com.ph>
2020-04-17 19:25:21 +0800

    done create article, added edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,29 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    #render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was successfully created."
+      #redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
