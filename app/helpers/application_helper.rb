010ecd23fcd337737c943f593d891af6d550f465
Emman Ruaza <emmanuel.ruaza@bibo.com.ph>
2020-04-27 13:23:52 +0800

    clean styling layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def active_links(link_text, link_path)
-    link_to link_text, link_path, class: "#{'active' if current_page?(link_path)}"
+  def active_links(link_text, link_path, extra_class)
+    link_to link_text, link_path, class: "#{'active' if current_page?(link_path)} #{extra_class}"
   end
 
 end
