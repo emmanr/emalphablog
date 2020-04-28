47b8d58a61e6f630142a456829412f0799649755
Emman Ruaza <emmanuel.ruaza@bibo.com.ph>
2020-04-29 01:18:26 +0800

    added bcrypt and password column to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save: { self.email = email.downcase }
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true,
                       uniqueness: { case_sensitive: false },
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
---
+++ db/migrate/20200428170535_add_userpassword_to_users.rb
@@ -0,0 +1,5 @@
+class AddUserpasswordToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
