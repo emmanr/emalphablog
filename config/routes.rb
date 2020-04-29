aa078b3afc71fcd29a69fb83ad527eccd5cf2570
Emman Ruaza <emmanuel.ruaza@bibo.com.ph>
2020-04-29 13:54:10 +0800

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update]
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
