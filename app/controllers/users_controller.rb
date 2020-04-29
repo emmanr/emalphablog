97586e677a5a780502c66fead5b04637d5e2b362
Emman Ruaza <emmanuel.ruaza@bibo.com.ph>
2020-04-29 14:27:47 +0800

    added update and edit for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -13,9 +15,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Successfully updated information for user #{@user.username}."
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
