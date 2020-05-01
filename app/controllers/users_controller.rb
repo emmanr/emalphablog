9f2bb2845668dad4bec7bdd9cfc663a29e49a281
Emman Ruaza <emmanuel.ruaza@bibo.com.ph>
2020-05-01 12:58:45 +0800

    added session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome #{@user.username}. You've registered Successfully."
       redirect_to articles_path
     else
